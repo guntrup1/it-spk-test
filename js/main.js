7a81d8004b8785ca5f6776d5368e69a6f409218d
guntrup1 <98844595+guntrup1@users.noreply.github.com>
2022-02-02 08:49:27 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$('.header__burger').click(function(event) {
+	$('.header__burger, .header__menu').toggleClass('active');
+	$('body').toggleClass('lock');
+
+	$(".menu__item").click(function(){
+    	$(".header__burger, .header__menu").removeClass("active");
+    	$("body").removeClass("lock");
+  	});
+});
